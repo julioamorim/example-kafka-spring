20016042e6fe5675032146b2e0463d15e5f3bcce
Júlio Amorim <julioamorim@MacBook-Pro-de-Julio.local>
2021-05-09 22:35:19 -0300

    First commit, adding initial dependencies.

---
+++ src/main/java/io/github/julioamorim/examplekafkaspring/ExampleKafkaSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.julioamorim.examplekafkaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleKafkaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleKafkaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/julioamorim/examplekafkaspring/ExampleKafkaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.julioamorim.examplekafkaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleKafkaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
